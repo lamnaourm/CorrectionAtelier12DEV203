be0889c0f7942fd54a9a27cd9adb94773842c814
lamnaourm <mohammed.lamnaour@gmail.com>
2023-03-08 15:21:59 +0100

    produit

---
+++ order-service/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const amqp = require('amqplib')
+const orderModel = require('./models/Order')
+
+const app = express();
+let connection, channel;
+const queueName1 = "order-service-queue";
+const queueName2 = "product-service-queue";
+
+app.use(express.json());
+
+mongoose
+  .connect(`mongodb://localhost:27017/dborders`, { useNewUrlParser: true })
+  .then(() => console.log("connexion BD reussie"))
+  .catch((error) => console.log('Erreur de connexion' + error));
+
+async function connectToRabbitMQ() {
+  const amqpServer = "amqp://guest:guest@localhost:5672";
+  connection = await amqp.connect(amqpServer);
+  channel = await connection.createChannel();
+  await channel.assertQueue(queueName1);
+  await channel.assertQueue(queueName2);
+}
+
+connectToRabbitMQ().then(() => {
+    channel.consume(queueName1, (data) => {
+        const listprods = JSON.parse(data.content.toString());
+        let total = 0.0;
+        listprods.forEach(item => {
+            total += item.price;
+        })
+        const order = new orderModel({products:listprods, total});
+        order.save();
+
+        
+        channel.ack(data);
+    })
+})
+
+app.listen(3001, ()=> {
+    console.log('serveur lance');
+})
--- product-service/routes/product.js
+++ product-service/routes/product.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const ProductModel = require("../models/Product");
+const amqp = require("amqplib");
 
 const routes = express.Router();
+
+let connection, channel;
+const queueName1 = "order-service-queue";
+const queueName2 = "product-service-queue";
+
+async function connectToRabbitMQ() {
+  const amqpServer = "amqp://guest:guest@localhost:5672";
+  connection = await amqp.connect(amqpServer);
+  channel = await connection.createChannel();
+  await channel.assertQueue(queueName1);
+  await channel.assertQueue(queueName2);
+}
+connectToRabbitMQ();
 
 routes.post("/", (req, res) => {
   const product = new ProductModel(req.body);
@@ -16,6 +30,12 @@ routes.post("/", (req, res) => {
     });
 });
 
-routes.post("/buy", (req, res) => {});
+routes.post("/buy", (req, res) => {
+    ProductModel.find({_id: {$in:req.body}}).then((products) => {
+        channel.sendToQueue(queueName1, Buffer.from(JSON.stringify(products)));
+    })
+
+    res.sendStatus(200);
+});
 
 module.exports = routes;
